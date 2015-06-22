8667580b704d4282140b67902856b3a0a5a8f6c4
Michael Bowen <mikee.bowen@gmail.com>
2015-06-22 15:26:00 -0700

    create basic server that can respond with console.log

--- server.js
+++ server.js
@@ -6,3 +6,18 @@ var app = express();
 var port = process.env.PORT || 3000;
 var time = new Date();
 var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error: '));
+db.on('open', function () {
+  console.log('database connection made');
+});
+
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/stories_development');
+
+app.get('/', function (req, res) {
+  res.send('Server can load a page');
+});
+
+app.listen(port, function() {
+  console.log('Server started on port: ' + port + '\n' + time)
+})
