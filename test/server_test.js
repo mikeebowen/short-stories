ebde6e19e66d3f26d2d01cdd148181b4007e6f02
Michael Bowen <mikee.bowen@gmail.com>
2015-06-26 15:38:19 -0700

    add tests for show by category route

--- test/server_test.js
+++ test/server_test.js
@@ -27,7 +27,7 @@ describe('Test Story Routes', function (done) {
   it('should create a new story with a post request to /api/stories', function (done) {
     chai.request('localhost:3000')
     .post('/api/stories')
-    .send({author: 'test author', categories: ['fiction', 'science fiction'], storyTitle: 'Test Title', storyText: 'test story text, blah blah blah something something'})
+    .send({author: 'test author', categories: ['fiction', 'sciencefiction'], storyTitle: 'Test Title', storyText: 'test story text, blah blah blah something something'})
     .end(function (err, res) {
       expect(err).to.eql(null);
       expect(res.body.author).to.eql('test author');
@@ -44,8 +44,22 @@ describe('Test Story Routes', function (done) {
     .end(function (err, res) {
       expect(err).to.eql(null);
       expect(res.body.length).to.eql(1);
-      expect(res.body[0]).to.eql('test story text, blah blah blah something something');
+      expect(res.body[0]).to.eql('Test Titletest authortest story text, blah blah blah something something');
       done();
+    });
+  });
+
+  it('should show stories by category', function (done) {
+    chai.request('localhost:3000')
+    .get('/api/stories/fiction')
+    .end(function (err, res) {
+      expect(err).to.eql(null)
+      expect(Array.isArray(res.body)).to.eql(true);
+      expect(res.body[0].author).to.eql('test author');
+      expect(res.body[0].categories).to.be.an('array');
+      expect(res.body[0].storyTitle).to.eql('Test Title');
+      expect(res.body[0].storyText).to.eql('test story text, blah blah blah something something');
+      done()
     });
   });
 
