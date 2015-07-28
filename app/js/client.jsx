c979d0ad2306407474bf0fcdf4f3a879d012591e
Michael Bowen <mikee.bowen@gmail.com>
2015-07-28 12:59:47 -0700

    try to make routes work

--- app/js/client.jsx
+++ app/js/client.jsx
@@ -1,6 +1,28 @@
+// 'use strict';
+
+// var React = require('react');
+// var App = require('./components/views/app.jsx');
+
+// React.render(<App />, document.body);
+
 'use strict';
 
 var React = require('react');
-var App = require('./components/views/app.jsx');
+var Router = require('react-router-component');
+var Locations = Router.Locations;
+var Location = Router.Location;
+var {MainPage} = require('./components/views/app.jsx');
 
-React.render(<App />, document.body);
+var App = React.createClass({
+
+  render: function() {
+    return (
+      <Locations>
+        <Location path="/" handler={MainPage} />
+      </Locations>
+    )
+  }
+})
+
+React.render(React.createElement(App), document.body)
+
